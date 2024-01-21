4495d56cea2b48554f657cde5458e0bb4bfd310b
Katie <thorburnkatie@gmail.com>
2024-01-21 20:40:45 +1100

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+let currentUser = "string";
+
+let welcomeMessage= `Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage);
+
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
